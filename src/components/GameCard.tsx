998a1a0139b158b2178a3da67129860a376ccfc8
Yahya Unlu <yhunlu87@gmail.com>
2023-12-01 22:29:32 +0300

    building game cards

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,21 @@
+import { Game } from '../hooks/useGames';
+import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+
+interface Props {
+  game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card borderRadius={10} overflow={'hidden'}>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize={'xl'} noOfLines={2}>
+          {game.name}
+        </Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
