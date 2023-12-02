68828516f8406ab3eec36cc6b78f5251bea46d23
Yahya Unlu <yhunlu87@gmail.com>
2023-12-02 22:29:34 +0300

    create a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,11 +5,9 @@ import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
 
-  const skeleton = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ];
+  const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
@@ -25,7 +23,7 @@ const GameGrid = () => {
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
           </GameCardContainer>
---
+++ src/hooks/useData.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import apiClient from '../services/api-client';
+import { CanceledError } from 'axios';
+
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+const useData = <T>(endpoint: string) => {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setIsLoading(true);
+    apiClient
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .then((res) => {
+        setData(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { data, error, isLoading };
+};
+
+export default useData;
