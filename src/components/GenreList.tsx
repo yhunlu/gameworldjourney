d3d33467d08fc3fcd3a53e5a5debdd3a96a9fffe
Yahya Unlu <yhunlu87@gmail.com>
2023-12-03 22:04:16 +0300

    highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,9 +6,10 @@ import { skeleton } from '../utils';
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   return (
@@ -27,7 +28,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
             />
             <Button
               onClick={() => onSelectGenre(genre)}
-              fontWeight="bold"
+              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
               fontSize={'lg'}
               variant={'link'}
             >
