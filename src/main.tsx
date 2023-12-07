ceac138cf5c9d824c157478413f0809601b7aea7
Yahya Unlu <yhunlu87@gmail.com>
2023-12-07 23:24:25 +0300

    add insights from vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import App from './App.tsx';
 import './index.css';
 import theme from './theme.ts';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <SpeedInsights />
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
