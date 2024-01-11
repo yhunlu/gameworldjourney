259d1c7b11d23034b53adb8b6bf46111f00d7827
yhunlu <yhunlu87@gmail.com>
2024-01-11 20:29:50 +0300

    breaking change, setting-up routing > > > Co-authored-by: John Moonshine <web3futureofweb@gmail.com> > Co-authored-by: Reucation <reucation@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import App from './App.tsx';
 import './index.css';
-import theme from './theme.ts';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { RouterProvider } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import router from './routes.tsx';
+import theme from './theme.ts';
 
 const queryClient = new QueryClient();
 
@@ -16,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <SpeedInsights />
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
