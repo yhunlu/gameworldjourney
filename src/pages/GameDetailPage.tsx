13a4afed4675d9d0950aa5a8bd842c7465ae0bbf
YAHYA UNLU <yhunlu87@gmail.com>
2024-01-12 20:43:36 +0300

    building expandable text, add brand color in theme

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,8 @@
+import { Heading } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
-import useGame from '../hooks/useGame';
+import ExpandableText from '../components/ExpandableText';
 import GameDetailSkeleton from '../components/GameDetailSkeleton';
-import { Heading, Text } from '@chakra-ui/react';
+import useGame from '../hooks/useGame';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -14,9 +15,9 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
-  )
+  );
 };
 
 export default GameDetailPage;
