cb16f87b02bb03b8707a807f2ef7ce90c0edacdc
YAHYA UNLU <yhunlu87@gmail.com>
2024-01-13 01:42:57 +0300

    build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -5,6 +5,7 @@ import GameAttributes from '../components/GameAttributes';
 import GameDetailSkeleton from '../components/GameDetailSkeleton';
 import useGame from '../hooks/useGame';
 import GameTrailer from '../components/GameTrailer';
+import GameScreenshots from '../components/GameScreenshots';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -20,6 +21,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
