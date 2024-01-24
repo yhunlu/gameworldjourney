e15e4d63682190e3a12373370814231767b56bd3
YAHYA UNLU <yhunlu87@gmail.com>
2024-01-24 17:34:45 +0300

    store in constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,4 @@ export const CACHE_KEY_PLATFORMS = 'platforms';
 export const CACHE_KEY_PLATFORMS_URL = 'platforms/lists/parents';
 export const CACHE_KEY_GAMES = 'games';
 export const CACHE_KEY_TAGS = 'tags';
+export const CACHE_KEY_STORES = 'stores';
