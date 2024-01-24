bda193d4f71d0c1346331d8c8bde19bf6bc46dc7
YAHYA UNLU <yhunlu87@gmail.com>
2024-01-24 20:18:46 +0300

    store component

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,4 +1,5 @@
 export { default as GameHeading } from './GameHeading';
 export { default as PlatformSelector } from './PlatformSelector';
 export { default as SortSelector } from './SortSelector';
+export { default as StoreSelector } from './StoreSelector';
 export { default as TagSelector } from './TagSelector';
