0784c808f4bf64caa050bac5e5c59089f9cc037c
YAHYA UNLU <yhunlu87@gmail.com>
2024-01-24 17:33:14 +0300

    redirect to homepage in backend.

--- src/components/home/TagSelector.tsx
+++ src/components/home/TagSelector.tsx
@@ -4,32 +4,32 @@ import { useTag, useTags } from '../../hooks';
 import useGameQueryStore from '../../store';
 
 const TagSelector = () => {
-    const { data } = useTags();
-    const setSelectedTagId = useGameQueryStore((s) => s.setTagId);
+  const { data } = useTags();
+  const setSelectedTagId = useGameQueryStore((s) => s.setTagId);
 
-    const selectedTagId = useGameQueryStore((s) => s.gameQuery.tagId);
-    const selectedTag = useTag(selectedTagId);
+  const selectedTagId = useGameQueryStore((s) => s.gameQuery.tagId);
+  const selectedTag = useTag(selectedTagId);
 
-    return (
-        <Menu>
-            <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-                {selectedTag?.name || 'Tags'}
-            </MenuButton>
-            <MenuList>
-                {data?.results.map((tag) => (
-                    <MenuItem
-                        fontWeight={tag.id === selectedTag?.id ? 'bold' : 'normal'}
-                        fontSize={tag.id === selectedTag?.id ? 'lg' : 'md'}
-                        onClick={() => setSelectedTagId(tag.id)}
-                        key={tag.id}
-                        cursor="pointer"
-                    >
-                        {tag.name}
-                    </MenuItem>
-                ))}
-            </MenuList>
-        </Menu>
-    );
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedTag?.name || 'Tags'}
+      </MenuButton>
+      <MenuList>
+        {data?.results.map((tag) => (
+          <MenuItem
+            fontWeight={tag.id === selectedTag?.id ? 'bold' : 'normal'}
+            fontSize={tag.id === selectedTag?.id ? 'lg' : 'md'}
+            onClick={() => setSelectedTagId(tag.id)}
+            key={tag.id}
+            cursor="pointer"
+          >
+            {tag.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
 };
 
 export default TagSelector;
